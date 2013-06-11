3998bcd1cb91265dc6faff3eb2617e968070e716
Andrea Wu <andreaowu@berkeley.edu>
2013-06-11 10:42:46 -0700

    fixed delete

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ io.configure(function () {
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'andreadb.cndfbjruyuqe.us-east-1.rds.amazonaws.com',
+    host     : 'rssdbinstance.cndfbjruyuqe.us-east-1.rds.amazonaws.com',
     user     : 'anwu',
     password : 'Aw2699a!',
-    database: 'andrea'
+    database: 'innodb'
 });
 connection.connect();
 
@@ -49,8 +49,7 @@ app.get('/', function (req, res) { // HTTP GET method with path /, "root" page
 
 io.sockets.on('connection', function (socket) {
     socket.on('get', function (id) {
-        console.log("get")
-        connection.query('SELECT * From Andrea WHERE id=' + id, function(err, rows, fields) {
+        connection.query('SELECT * FROM Andrea WHERE id=' + id, function(err, rows, fields) {
             if (err || rows.length == 0) {
                 socket.emit('error', "Queried ID does not exist");
             } else {
@@ -59,8 +58,7 @@ io.sockets.on('connection', function (socket) {
         });
     });
     socket.on('getAll', function () {
-        console.log("getAll")
-        connection.query('SELECT * From Andrea', function(err, rows, fields) {
+        connection.query('SELECT * FROM Andrea', function(err, rows, fields) {
             if (err) {
                 socket.emit('error', "Getting all records failed");
             } else {
@@ -69,7 +67,6 @@ io.sockets.on('connection', function (socket) {
         });
     });
     socket.on('put', function (id, name) {
-        console.log("put")
         connection.query('INSERT INTO Andrea VALUES (\'' + id + '\', \'' + name + '\')', function(err, rows, fields) {
             if (err || name == null || id == null) {
                 socket.emit('error', "Put failed");
@@ -79,7 +76,6 @@ io.sockets.on('connection', function (socket) {
         });
     });
     socket.on('update', function (id, name) {
-        console.log("update")
         connection.query('UPDATE Andrea SET name=\'' + name + '\' WHERE id=\'' + id + '\'', function(err, rows, fields) {
             if (err || rows.changedRows == 0 || name == null || id == null) {
                 socket.emit('error', "Update ID does not exist");
@@ -90,7 +86,7 @@ io.sockets.on('connection', function (socket) {
     });
     socket.on('delete', function (id) {
         connection.query('DELETE From Andrea WHERE id=' + id, function(err, rows, fields) {
-            if (err || rows.changedRows == 0 || id == null) {
+            if (err || rows.affectedRows == 0 || id == null) {
                 socket.emit('error', "Delete ID does not exist");
             } else {
                 socket.emit('response', "[{\"id\":" + id + "\"}]", "delete");
